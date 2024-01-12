fde732924a3b686cdb0c0bbb2a1bd13e9627cb30
tbray0 <112577397+tbray0@users.noreply.github.com>
2024-01-12 13:12:03 -0700

    test

--- config.js
+++
@@ -1,8 +0,0 @@
-/**
- * Open your spotify dashboard
- * https://beta.developer.spotify.com/dashboard
- */
-
-const client_id = 'aa86db865ba94d86b96c2cf4138b51f7';
-const redirect_uri = 'http://localhost:3000/callback';
-const client_secret = 'ddc5bb9f85d843418cdff0c238772db7';
